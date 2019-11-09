bc49a8d4a1101ffd1d77ca48399543005ca8a731
michaldobiezynski <dobiezynski@gmail.com>
2019-11-09 18:26:13 +0000

    rendered names of monsters in h1 tags

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     return (
         <div className="App">
           {
-            this.state.monsters.map
+            this.state.monsters.map(monster => <h1> { monster.name } </h1>)
           }
         </div>
     )
