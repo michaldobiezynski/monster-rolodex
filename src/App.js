f5bc9837cb36c6fc8f7597ce543037838b6ab360
michaldobiezynski <dobiezynski@gmail.com>
2019-11-11 20:56:25 +0000

    created a new state variable and I'm assigning a value of the search field to it on the change of the value of the search field

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-
-      ]
+      monsters: [],
+      searchField: '',
     }
   }
 
@@ -22,7 +21,10 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <input type='search' placeholder='search monsters' />
+          <input type='search' placeholder='search monsters'
+          onChange={ e =>
+            this.setState( { searchField: e.target.value })}
+          />
           <CardList monsters={this.state.monsters}>
           </CardList>
         </div>
