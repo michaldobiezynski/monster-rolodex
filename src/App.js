a3fea2270a789376a33e4f4c7301f08083ca401d
michaldobiezynski <dobiezynski@gmail.com>
2019-11-15 19:11:05 +0000

    added h1 title and google fonts

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
-    
+
   }
 
   componentDidMount() {
@@ -32,6 +32,7 @@ class App extends Component {
 
     return (
         <div className="App">
+            <h1>Monsters Rolodex</h1>
           <SearchBox
               placeholder='search monsters'
               handleChange={this.handleChange}
