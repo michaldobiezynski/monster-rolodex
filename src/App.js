1d61e95874ae5138d7feeeb5a7170442f467a3e2
michaldobiezynski <dobiezynski@gmail.com>
2019-11-15 18:42:17 +0000

    made bespoke function for handling change of the searchform - this means it is reusable

--- src/App.js
+++ src/App.js
@@ -19,17 +19,21 @@ class App extends Component {
         .then(users => this.setState({monsters: users}))
   }
 
+  handleChange(e) {
+      this.setState( { searchField: e.target.value })
+  }
+
   render() {
       const { monsters, searchField } = this.state;
       const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
       );
+
     return (
         <div className="App">
           <SearchBox
               placeholder='search monsters'
-              handleChange={e =>
-                  this.setState( { searchField: e.target.value })}
+              handleChange={this.handleChange()}
           />
           <CardList monsters={filteredMonsters}>
           </CardList>
